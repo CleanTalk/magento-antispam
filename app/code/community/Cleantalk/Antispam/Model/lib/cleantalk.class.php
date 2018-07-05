bd10784c66079189a2aa306b5ecec76675f9c48f
davydovct <davydov@cleantalk.org>
2018-07-05 21:52:31 +0500

    fix for apache_request_headers

--- app/code/community/Cleantalk/Antispam/Model/lib/cleantalk.class.php
+++ app/code/community/Cleantalk/Antispam/Model/lib/cleantalk.class.php
@@ -986,6 +986,33 @@ function noticePaidTill($api_key)
 	return $result;
 }
 
+/* 
+ * If Apache web server is missing then making
+ * Patch for apache_request_headers() 
+ */
+if(!function_exists('apache_request_headers'))
+{
+    function apache_request_headers()
+    {
+        $headers = array(); 
+        foreach($_SERVER as $key => $val){
+            if(preg_match('/\AHTTP_/', $key)){
+                $server_key = preg_replace('/\AHTTP_/', '', $key);
+                $key_parts = explode('_', $server_key);
+                if(count($key_parts) > 0 and strlen($server_key) > 2){
+                    foreach($key_parts as $part_index => $part){
+                        $key_parts[$part_index] = function_exists('mb_strtolower') ? mb_strtolower($part) : strtolower($part);
+                        $key_parts[$part_index][0] = strtoupper($key_parts[$part_index][0]);                    
+                    }
+                    $server_key = implode('-', $key_parts);
+                }
+                $headers[$server_key] = $val;
+            }
+        }
+        return $headers;
+    }
+}
+
 /**
  * Function sends raw request to API server
  *
